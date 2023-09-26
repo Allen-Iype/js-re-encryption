4e1df42274afeaa1ec2e1452cb563167355d2db5
Allen-Iype <allen.aipc7@gmail.com>
2023-09-26 16:50:58 +0530

    Fix input and response of apis

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const umbral = require("@nucypher/umbral-pre");
-const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,44 +7,70 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON payloads
 app.use(express.json());
 
+function bytesToHex(bytes) {
+  return Array.from(bytes, (byte) => {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('');
+}
+
+function hexToBytes(hex) {
+  const bytes = [];
+  for (let i = 0; i < hex.length; i += 2) {
+    bytes.push(parseInt(hex.substr(i, 2), 16));
+  }
+  return new Uint8Array(bytes);
+}
+
 app.get('/generate-secret-key', (req, res) => {
-  let secret_key = umbral.SecretKey.random();
-  const filePath = 'secret_key.txt';
-  fs.writeFileSync(filePath, secret_key.toBEBytes().toString('hex'));
+  let secretKey = umbral.SecretKey.random();
+  let secretKeyBytes = secretKey.toBEBytes(); //Secret key to Big Endian bytes as noted from the rust-umbral-pre documentation
+  console.log(secretKeyBytes)
+  let secretKeyHex = bytesToHex(secretKeyBytes)
+  console.log(secretKeyHex)
+  let publicKey = secretKey.publicKey().toCompressedBytes();
+  let publicKeyHex = bytesToHex(publicKey);
   
-  console.log("Secret Key Bytes:", secret_key.toBEBytes());
-  console.log("Secret Key Object:", secret_key);
-  const publicKey = secret_key.publicKey();
-  console.log("Public Key:", publicKey);  // Adjust based on available methods
-  
-  res.json({ secretKey: secret_key.toBEBytes().toString('hex') });
+  res.json({ secretKey: secretKeyHex, publicKey: publicKeyHex });
 });
 
 app.post('/encrypt', (req, res) => {
     let { public_key, plaintext } = req.body;
+    console.log(req.body)
     let plaintext_bytes = Buffer.from(plaintext, 'utf8');
+    let publicKeyBytes = hexToBytes(public_key);
+    console.log(`Public key is ${public_key}`);
+    let publicKey = umbral.PublicKey.fromCompressedBytes(publicKeyBytes);
+
   
-    if (!public_key || !plaintext_bytes) {
+    if (!publicKey || !plaintext_bytes) {
       return res.status(400).json({ error: 'Missing required parameters' });
     }
   
-    const [capsule, ciphertext] = umbral.encrypt(public_key, plaintext_bytes);
-    return res.json({ capsule, ciphertext });
+    const [capsule, ciphertext] = umbral.encrypt(publicKey, plaintext_bytes);
+    let capsuleBytes = capsule.toBytes();
+    let capsuleHex = bytesToHex(capsuleBytes);
+    let cipherTextHex = bytesToHex(ciphertext)
+    return res.json({ capsule: capsuleHex, ciphertext: cipherTextHex });
 });
 
 app.post('/decrypt', (req, res) => {
-    const { capsule, ciphertext } = req.body;
+    let dec = new TextDecoder("utf-8");
+    const { secretKey, capsule, ciphertext } = req.body;
+    let secretKeyBytes = hexToBytes(secretKey)
+    let capsuleBytes = hexToBytes(capsule)
+    let ciphertextBytes = hexToBytes(ciphertext)
+    let capsuleObtained = umbral.Capsule.fromBytes(capsuleBytes)
+    console.log("Secret Key recreated Bytes:", secretKeyBytes)
+    let secretKeyObtained = umbral.SecretKey.fromBEBytes(secretKeyBytes);
+    console.log(secretKeyObtained)
   
-    // Read and parse the secret key from the file
-    const fileContent = fs.readFileSync('secret_key.txt', 'utf8');
-    const secretKeyBytes = Buffer.from(fileContent, 'hex');
-    const secret_key = umbral.SecretKey.fromBEBytes(secretKeyBytes);
-  
-    if (!secret_key || !capsule || !ciphertext) {
+    if (!secretKeyObtained || !capsuleObtained || !ciphertext) {
       return res.status(400).json({ error: 'Missing required parameters' });
     }
   
-    const plaintext = umbral.decryptOriginal(secret_key, capsule, ciphertext);
+    let plaintextBytes = umbral.decryptOriginal(secretKeyObtained, capsuleObtained, ciphertextBytes);
+    let plaintext = dec.decode(plaintextBytes)
+
     return res.json({ plaintext });
 });
 
